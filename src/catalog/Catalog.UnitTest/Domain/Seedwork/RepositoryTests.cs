4006515eb7aeeb5db402e59e15d2b3f6f061ffb8
Neosoft <neosoft@Neosofts-MacBook-Pro.local>
2023-02-02 20:06:21 +0530

    added repository test

---
+++ src/catalog/Catalog.UnitTest/Domain/Seedwork/RepositoryTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Catalog.Domain.SeedWork;
+
+namespace Catalog.UnitTest.Domain.Seedwork
+{
+	public class RepositoryTests
+	{
+		[Fact]
+		public void WithUnitOfWorkSetInStubRepository_GetUnitOfWorkShouldReturnUnitOfWork()
+		{
+			//Arrange 
+			var stubRepo = new StubRepository()
+			{
+				UnitOfWork = new MockUnitOfWork()
+			};
+
+			//Act
+
+			//Assert
+			Assert.IsType<MockUnitOfWork>(stubRepo.UnitOfWork);
+		}
+
+        [Fact]
+        public async void WithUnitOfWorkSetInStubRepository_SaveChangesAsyncShouldReturnTrue()
+        {
+            //Arrange 
+            var stubRepo = new StubRepository()
+            {
+                UnitOfWork = new MockUnitOfWork()
+            };
+
+			//Act
+			var result = await stubRepo.UnitOfWork.SaveChangesAsync();
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+
+    class MockEntity3 : Entity, IAggregateRoot
+    {
+
+    }
+
+	class StubRepository : IRepository<MockEntity3>
+	{
+		
+	}
+
+	class MockUnitOfWork : IUnitOfWork
+	{
+		public void Dispose()
+		{
+			
+		}
+
+		public Task<bool> SaveChangesAsync(CancellationToken cancellationToken = default)
+		{
+			return Task.FromResult(true);
+		}
+	}
+}
