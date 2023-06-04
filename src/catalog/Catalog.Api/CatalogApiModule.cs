d709e92ff8f1f38746906cfba2f81a4d47e1938a
rajgog <raj.gogri+github@gmail.com>
2023-06-04 14:56:18 +0530

    fixed catalog host project

--- src/catalog/Catalog.Api/CatalogApiModule.cs
+++ src/catalog/Catalog.Api/CatalogApiModule.cs
@@ -2,23 +2,30 @@
 using Catalog.Api.Services;
 using Catalog.Application;
 using Volo.Abp;
+using Volo.Abp.AspNetCore;
+using Volo.Abp.AspNetCore.Mvc;
 using Volo.Abp.Autofac;
 using Volo.Abp.Modularity;
 
 namespace Catalog.Api
 {
-	[DependsOn(typeof(AbpAutofacModule),
+	[DependsOn(
+        typeof(AbpAspNetCoreMvcModule),
+        typeof(AbpAutofacModule),
         typeof(CatalogApplicationModule))]
 	public class CatalogApiModule : AbpModule
 	{
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            context.Services.AddGrpc();
+            context.Services.AddGrpc((opt) => {
+                opt.EnableDetailedErrors = true;
+            });
         }
 
         public override void OnApplicationInitialization(ApplicationInitializationContext context)
         {
             var app = context.GetApplicationBuilder();
+            app.UseRouting();
             app.UseConfiguredEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
--- src/catalog/Catalog.Api/Program.cs
+++ src/catalog/Catalog.Api/Program.cs
@@ -3,14 +3,8 @@ using Catalog.Api.Services;
 using Volo.Abp;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Host.UseAutofac();
+builder.Host.UseAutofac(); // Integrate Autofac!
 await builder.AddApplicationAsync<CatalogApiModule>();
-
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
 var app = builder.Build();
 await app.InitializeApplicationAsync();
 await app.RunAsync();
-//app.Run();
