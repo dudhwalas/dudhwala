8365d484ec27611f7dec94f77be25e29de3105f6
Neosoft <neosoft@Neosofts-MacBook-Pro.local>
2023-06-19 19:42:45 +0530

    added service checks

--- src/catalog/Catalog.Application/Services/BrandService.cs
+++ src/catalog/Catalog.Application/Services/BrandService.cs
@@ -16,17 +16,15 @@ namespace Catalog.Application.Services
         private readonly IRepository<Brand,Guid> _brandRepo;
         private readonly IObjectMapper _objMapper;
         private readonly BrandManager _brandManager;
-        private readonly IGuidGenerator _guidGenerator;
 
         public BrandService(IRepository<Brand,Guid> brandRepo, IObjectMapper objMapper, BrandManager brandManager)
 		{
             _brandRepo = brandRepo;
             _objMapper = objMapper;
             _brandManager = brandManager;
-            _guidGenerator = _brandManager.LazyServiceProvider.LazyGetService<IGuidGenerator>();
         }
 
-        public override async Task<BrandDto> GetBrand(GetBrandRequest request, ServerCallContext context)
+        public override async Task<BrandDto> GetBrand(GetBrandRequestDto request, ServerCallContext context)
         {
             try
             {
@@ -44,10 +42,22 @@ namespace Catalog.Application.Services
         }
 
         [UnitOfWork]
-        public override async Task<BrandDto> AddBrand(BrandDto request, ServerCallContext context)
+        public override async Task<BrandDto> AddBrand(CreateBrandRequestDto request, ServerCallContext context)
         {
-            var createdBrand = await _brandManager.CreateAsync(request.Name, request.Image, (EnumStatus)request.Status, _guidGenerator.Create());
-            return _objMapper.Map<Brand,BrandDto>(createdBrand);
+            try
+            {
+                var realmId = Guid.Parse(request.RealmId);
+                var createdBrand = await _brandManager.CreateAsync(request.Name, request.Image, (EnumStatus)request.Status, realmId);
+                return _objMapper.Map<Brand, BrandDto>(createdBrand);
+            }
+            catch (FormatException ex)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, ex.Message));
+            }
+            catch (ArgumentException ex)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, ex.Message));
+            }
         }
     }
 }
