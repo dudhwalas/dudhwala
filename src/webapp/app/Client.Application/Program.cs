7ac2471f236d10bb149420c642b5a0fd3e161c64
Mac <mac@Macs-MacBook-Pro.local>
2024-01-09 20:29:15 +0530

    added fluentui

--- src/webapp/app/Client.Application/Program.cs
+++ src/webapp/app/Client.Application/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client.Application;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddFluentUIComponents();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
