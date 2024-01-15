e545741a7243f0170e9db415499c155e871ae37c
Mac <mac@Macs-MacBook-Pro.local>
2024-01-15 20:57:10 +0530

    added auth

--- src/webapp/app/Client.Application/Program.cs
+++ src/webapp/app/Client.Application/Program.cs
@@ -7,7 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddFluentUIComponents();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth", options.ProviderOptions);
+});
 await builder.Build().RunAsync();
