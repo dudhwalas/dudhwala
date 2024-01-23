784e6c5e5eaae95ac7be75de3f0c11d0ead1ecf6
rajgog <98525852+rajgog@users.noreply.github.com>
2024-01-23 23:35:58 +0530

    Ft-catalog (#2)
    * added menu items
    
    * added check to handle null
    
    * added module pages
    
    ---------
    
    Co-authored-by: Mac <mac@Macs-MacBook-Pro.local>

--- src/webapp/app/Client.Application/Constants.cs
+++ src/webapp/app/Client.Application/Constants.cs
@@ -1,6 +1,11 @@
-﻿namespace Client.Application;
-
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+namespace Client.Application;
 public static class Constants
 {
     public static string APP_NAME = "x x x"; 
+    public static string MODULE_CATALOG = "Catalog";
+    public static string MODULE_DELIVERY_SQUAD = "Delivery Squad";
+    
+    public static Emoji EMOJI_CATALOG = new Emojis.Objects.Flat.Default.ShoppingBags();
+    public static Emoji EMOJI_DELIVERY_SQUAD = new Emojis.PeopleBody.Flat.Default.ConstructionWorker();
 }
